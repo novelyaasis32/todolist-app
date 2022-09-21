0ae456328933d42394ecfddd71706f8e669f815b
novelyaasis32 <novelya32@gmail.com>
2022-09-21 21:09:15 +0700

    progress api

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
